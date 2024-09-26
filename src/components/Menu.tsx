e522eeff3d80fe05bb1ad4c356f003a8450b4222
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-09-25 21:33:18 -0300

    feat: Add menu toggle functionality and update menu links
    - Added functionality to toggle the menu open and close
    - Updated menu links to use dynamic data from an array
    - Added icons for opening and closing the menu
    - Updated alt text for menu icons
    - Updated login/orders link to be dynamic based on user authentication status
    - Added link for the shopping cart

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -15,64 +15,38 @@ const links = [
 const Menu = () => {
   const [open, setOpen] = useState(false);
 
-  // TEMPORARY
+  // TEMPORARY: Simulación de usuario no autenticado
   const user = false;
+
   return (
     <div>
-      {/* LONG WAY */}
-      {/* {!open ? (
-        <Image
-          src="/open.png"
-          alt=""
-          width={20}
-          height={20}
-          onClick={() => setOpen(true)}
-        />
-      ) : (
-        <Image
-          src="/close.png"
-          alt=""
-          width={20}
-          height={20}
-          onClick={() => setOpen(false)}
-        />
-      )} */}
-      
-      {/* SHORTCUT */}
+      {/* Icono de abrir/cerrar menú */}
       <Image
         src={open ? "/close.png" : "/open.png"}
-        alt=""
+        alt={open ? "Cerrar menú" : "Abrir menú"}
         width={20}
         height={20}
         onClick={() => setOpen(!open)}
         className="cursor-pointer"
       />
+      
       {open && (
         <div className="bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] flex flex-col gap-8 items-center justify-center text-3xl z-10">
-          {links.map((item) => (
-            <Link href={item.url} key={item.id} onClick={() => setOpen(false)}>
-              {item.title}
+          {links.map(({ id, title, url }) => (
+            <Link href={url} key={id} onClick={() => setOpen(false)}>
+              {title}
             </Link>
           ))}
 
-          {/* LONG WAY */}
-          {/* {!user ? (
-            <Link href="/login" onClick={() => setOpen(false)}>
-              Login
-            </Link>
-          ) : (
-            <Link href="/orders" onClick={() => setOpen(false)}>
-              Orders
-            </Link>
-          )} */}
-
-          {/* SHORTCUT */}
+          {/* Login/Orders Link */}
           <Link
-            href={user ? "/orders" : "login"}
+            href={user ? "/orders" : "/login"}
             onClick={() => setOpen(false)}
           >
             {user ? "Orders" : "Login"}
           </Link>
+
+          {/* Link para el carrito */}
           <Link href="/cart" onClick={() => setOpen(false)}>
             <CartIcon />
           </Link>
